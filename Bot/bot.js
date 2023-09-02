92c453de9e0505ad41449faa41e5649d2ca1da24
Egor Zhurbitskiy <136472117+Goga2112@users.noreply.github.com>
2023-09-02 16:21:34 +0300

    Bing + Google superbot v.0.4

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,10 +1,11 @@
 // ==UserScript==
-// @name         Bot for Bing
+// @name         Bot for Bing & Google
 // @namespace    http://tampermonkey.net/
-// @version      0.3
-// @description  Bot for Bing
+// @version      0.4
+// @description  Bot for Bing & Google
 // @author       Zhurbitskiy Egor
 // @match        https://www.bing.com/*
+// @match        https://www.google.com/*
 // @match        https://dzen.ru/*
 // @match        https://habr.com/*
 // @match        https://discovery-russia.ru/*
@@ -12,34 +13,48 @@
 // ==/UserScript==
 
 let links = document.links;
-let btnK = document.getElementsByClassName("search")[0];
-let btnN = document.querySelector("a[title$='Следующая страница']");
-let bingInput = document.getElementsByName("q")[0];
+let btnB = document.getElementsByClassName("search")[0];
+let btnG = document.getElementsByName("btnK")[0];
+let btnNB = document.querySelector("a[title$='Следующая страница']");
+let btnNG = document.getElementById('pnnext');
+let input = document.getElementsByName("q")[0];
 let sites = {
-  "dzen.ru":["зауроподы","ТОП-10 лучших фильмов всех времен","Корабли в советской фантастике"],
+  "dzen.ru":["Зауроподы","ТОП-10 лучших фильмов всех времен","Корабли в советской фантастике"],
   "habr.com":["Учим три телевизора делать солнышко", "Cheetah", "Абстрактные типы данных"],
-  "discovery-russia.ru":["СТРАНЫ И ГОРОДА В ЮЖНОЙ АМЕРИКЕ", "ЧТО ПОСМОТРЕТЬ В КОРКЕ", "АВСТРАЛИЯ И ОКЕАНИЯ"]
-}
+  "discovery-russia.ru":["страны и города в южной Америке", "что посмотреть в Корке", "Австралия и Океания"]
+};
+let searchMachines = ["https://www.bing.com/", "https://www.google.com/"];
+let searchMachine = searchMachines[getRandom(0, searchMachines.length)];
 let site = Object.keys(sites)[getRandom(0, Object.keys(sites).length)];
 let keywords = sites[site];
 let keyword = keywords[getRandom(0, keywords.length)];
 
-if (btnK != undefined) {
+if (btnB != undefined || btnG != undefined) {
   document.cookie = `site=${site}`;
-} else if (location.hostname == "www.bing.com") {
+} else if (location.hostname == "www.bing.com" || location.hostname == "www.google.com" ) {
   site = getCookie("site");
 } else {
   site = location.hostname;
 };
 
-if (btnK != undefined) {
+if (btnB != undefined) {
   let i = 0;
   let timerId = setInterval(function() {
-    bingInput.value += keyword[i];
+    input.value += keyword[i];
     i++;
     if (i == keyword.length) {
       clearInterval(timerId);
-      btnK.click();
+      btnB.click();
+    };
+  }, 700)
+  }else if (btnG != undefined) {
+  let i = 0;
+  let timerId = setInterval(function() {
+    input.value += keyword[i];
+    i++;
+    if (i == keyword.length) {
+      clearInterval(timerId);
+      btnG.click();
     };
   }, 700)
   } else if (location.hostname == site) {
@@ -47,7 +62,7 @@ if (btnK != undefined) {
     setInterval(() => {
       let index = getRandom(0, links.length); 
       if (getRandom(0, 101) >= 75) {
-        location.href = "https://www.bing.com/";
+        location.href = searchMachine;
       };
       if (links.length == 0) {
         location.href = site;
@@ -58,30 +73,42 @@ if (btnK != undefined) {
     }, getRandom(3000, 5000))
   } else {
     let nextBingPage = true;
+    let nextGooglePage = true;
     for (let i = 0; i < links.length; i++) {
       if(links[i].href.indexOf(site) != -1) {
         console.log("Нашел строку " + links[i]);
         let link = links[i];
         nextBingPage = false;
+        nextGooglePage = false;
         setTimeout(() => { link.click();}, getRandom(2000, 4000));
         break;
       };
     };
     let elementExist = setInterval(() => {
-      let elm = document.querySelector("a[class$='sb_pagS sb_pagS_bp b_widePag sb_bp']");
-      if (elm != null) {
-
-        if (elm.firstChild.data == '5') {
+      let elmB = document.querySelector("a[class$='sb_pagS sb_pagS_bp b_widePag sb_bp']");
+      let elmG = document.querySelector(".YyVfkd");
+      if (elmB != null) {
+        if (elmB.firstChild.data == '5') {
           nextBingPage = false;
-          location.href = "https://www.bing.com/";
+          location.href = searchMachine;
+        };
+        clearInterval(elementExist);
+      }else if (elmG != null) {
+        if (elmG.innerText == "5") {
+          nextGooglePage = false;
+          location.href = searchMachine;
         };
         clearInterval(elementExist);
       };
-    }, 100)
-
+    }, 100);
     if (nextBingPage) {
       setTimeout(() => {
-        btnN.click();
+        btnNB.click();
+      }, getRandom(3500, 5500))
+    };
+    if (nextGooglePage) {
+      setTimeout(() => {
+        btnNG.click();
       }, getRandom(3500, 5500))
     };
   };
